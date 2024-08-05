127627d3d1e5bf75a9261ca974e4a505703d3877
Wojciech Mazur <wojtekemmm@gmail.com>
2024-08-05 10:31:28 +0200

    controllers

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -2,9 +2,72 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Answer;
+use App\Http\Requests\StoreAnswerRequest;
+use App\Http\Requests\UpdateAnswerRequest;
+use App\Models\Attempt;
+use App\Models\Survey;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class AnswerController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        return Inertia::render('Answers/Index', [
+            'answers' => Answer::with(['question', 'option'])->paginate(10),
+            'survey' => Survey::query()->get()->first()
+         ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAnswerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAnswerRequest $request, Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Answer $answer)
+    {
+        //
+    }
 }
--- app/Http/Controllers/AttemptController.php
+++ app/Http/Controllers/AttemptController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Attempt;
+use App\Models\Survey;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class AttemptController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        return Inertia::render('Attempts/Index', [
+            'attempts' => Attempt::list(),
+            'survey' => Survey::list()
+         ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): Response
+    {
+        $survey = Survey::query()->with('questions.options')->get()->first();
+
+        return Inertia::render('Attempts/Create', [
+            'attempt' => Attempt::create([
+                'survey_id' => $survey->id
+            ]),
+            'survey' => $survey
+        ]);
+    }
 }
