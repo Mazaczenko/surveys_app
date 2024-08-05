280d438ba60ced9c3588c33fc4e921a26e91ff96
Wojciech Mazur <wojtekemmm@gmail.com>
2024-08-05 12:40:37 +0200

    seeders and migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-        $this->call(SurveyAnswersSeeder::class);
         $this->call(SurveyWithQuestionsSeeder::class);
+        $this->call(SurveyAnswersSeeder::class);
     }
 }
