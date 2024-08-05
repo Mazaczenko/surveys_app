9f1e6f94c28d3fbf82e475030251f7f7e29a7687
Wojciech Mazur <wojtekemmm@gmail.com>
2024-08-05 12:53:24 +0200

    layout X icons

--- resources/js/Layouts/SurveyLayout.vue
+++ resources/js/Layouts/SurveyLayout.vue
@@ -1,13 +1,15 @@
-<template>
-
-</template>
-
-<script>
-export default {
-    name: "SurveyLayout"
-}
+<script setup>
+    import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 </script>
 
-<style scoped>
+<template>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div>
+            <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+        </div>
 
-</style>
+        <div class="w-full mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <slot />
+        </div>
+    </div>
+</template>
